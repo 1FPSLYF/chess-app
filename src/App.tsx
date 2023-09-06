4a6a8a1b553b6831035e5cc2bcfd52977524b567
Harsh <04harshvardhan@gmail.com>
2023-09-06 18:20:06 +0530

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 import Box from './Box';
+
+const arr = [1,2,3,4,5,6,7,8];
 
 function App() {
   return (
-    <div>
-      <Box/>
+    <div className='flex flex-row'>
+      {
+        arr.map((index)=>{
+          return <Box color = 'bg-red-300' key = {index}/>
+        })
+      }
+      <Box color='bg-red-300'/>
+      <Box color='bg-stone-700'/>
+      <Box color='bg-red-300'/>
     </div>
   );
 }
--- src/Box.tsx
+++ src/Box.tsx
@@ -1,4 +1,4 @@
-function Box(){
-return<div></div>
+function Box({color}:any){
+return<div className={`w-16 h-16 ${color}`}></div>
 }
 export default Box;
