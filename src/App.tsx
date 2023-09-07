95883a8c50721fae11661d5269420f06699b8d28
Harsh <04harshvardhan@gmail.com>
2023-09-07 11:06:17 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,25 @@ const arr = [1,2,3,4,5,6,7,8];
 
 function App() {
   return (
-    <div className='flex flex-row'>
+    <div className='flex flex-col items-center'>
       {
-        arr.map((index)=>{
-          return <Box color = 'bg-red-300' key = {index}/>
-        })
+        arr.map((index1)=>{
+          return <div className='flex flex-row' key = {index1}>
+          {
+              arr.map((index)=>{
+                let t ,u, pass;
+                t= index1%2===1 ? 'bg-red-300' : 'bg-stone-700';
+                u = index1%2===1 ? 'bg-stone-700' : 'bg-red-300';
+                pass = index%2===1 ? t : u;
+                return <Box color = {pass} key = {index}/>
+               })
+          }
+        </div>
+        })  
       }
-      <Box color='bg-red-300'/>
-      <Box color='bg-stone-700'/>
-      <Box color='bg-red-300'/>
     </div>
   );
 }
 
 export default App;
+
